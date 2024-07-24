dcd666352b98a37271391f20835eb987d3f2d76a
metombega <metombega@gmail.com>
2024-07-24 17:34:42 -0400

    find workers that could be replaced

--- calender.py
+++ calender.py
@@ -6,7 +6,7 @@ experties = ['a', 'b', 'c', 'd']
 workers_experties = {
     'Alice': ['a', 'b', 'c'], 
     'Bob': ['c', 'b'], 
-    'Charlie': ['a', 'd'], 
+    'Charlie': ['a', 'b', 'd'], 
     'David': ['b', 'c'], 
     'Eve': ['a', 'b', 'c', 'd'], 
     'Frank': ['a', 'b', 'c', 'd'], 
@@ -16,10 +16,10 @@ workers_experties = {
 }
 
 workers_availability = {
-    'Alice': ['2/1/2025', '3/1/2025', '4/1/2025', '5/1/2025'], 
+    'Alice': ['1/1/2025', '2/1/2025', '3/1/2025', '4/1/2025', '5/1/2025'], 
     'Bob': ['5/1/2025', '6/1/2025'], 
     'Charlie': ['1/1/2025',  '3/1/2025', '4/1/2025', '5/1/2025', '6/1/2025'], 
-    'David': ['1/1/2025', '2/1/2025', '3/1/2025', '4/1/2025', '5/1/2025', '6/1/2025'], 
+    'David': ['1/1/2025', '2/1/2025', '3/1/2025', '5/1/2025', '6/1/2025'], 
     'Eve': ['1/1/2025', '2/1/2025',  '5/1/2025', '6/1/2025'], 
     'Frank': [ '2/1/2025', '3/1/2025', '4/1/2025', '5/1/2025', '6/1/2025'], 
     'Grace': ['1/1/2025', '2/1/2025', '3/1/2025', '4/1/2025', '5/1/2025', '6/1/2025'], 
@@ -41,55 +41,38 @@ workers_max_work_days = {
 mandatory_experties = {'a': 1, 'b': 2, 'c': 1, 'd': 0} # maybe should change according to the date
 num_of_workers_per_day = 5
 
-def find_workers_for_date(date):
-    # mandatory_experties_copy = mandatory_experties.copy() 
-    workers_for_date = []
-    for date in activity_dates:
-        # find available workers for this date
-        available_workers = []
-        for worker in workers:
-            if date in workers_availability[worker]:
-                available_workers.append(worker)
-        # # find the workers for the activity
-        # for experty in mandatory_experties_copy:
-        #     if mandatory_experties_copy[experty] > 0:
-        #         for worker in available_workers:
-        #             if experty in workers_experties[worker]:
-        #                 mandatory_experties_copy[experty] -= 1
-        #                 workers_for_date.append(worker)
-        #                 available_workers.remove(worker)
-    return workers_for_date
-
-def find_workers_with_special_experties():
-    workers_with_special_experties = []
-    for worker in workers:
-        if len(workers_experties[worker]) == len(experties):
-            workers_with_special_experties.append(worker)
-    return workers_with_special_experties
-
 def schedual_workers():
-    for date in activity_dates:
-        # sort workers by the max work days devided by the number of days they can work. pay attention to devided by zero
-        workers_sorted = sorted(workers, key=lambda worker: workers_max_work_days[worker]/len(workers_availability[worker]) if len(workers_availability[worker]) > 0 else workers_max_work_days[worker])
-        # workers_sorted = sorted(workers, key=lambda worker: workers_max_work_days[worker]/len(workers_availability[worker]))
+    """ 
+    sort the dates by the number of workers that can work on this date - we want to take first the dates with the least workers
+    sort the workers by the max number of days they can work devide by the number of their available days
+    """
+    schedual = {}
+    # sort the dates by the number of workers that can work on this date
+    sorted_activity_dates = sorted(activity_dates, key=lambda date: len([worker for worker in workers if date in workers_availability[worker]]))
+    for date in sorted_activity_dates:
+        schedual[date] = {'workers': [], 'replacble_workers': []}
         # filter the workers that can work in this date
-        workers_sorted = [worker for worker in workers_sorted if date in workers_availability[worker]]
+        available_workers = [worker for worker in workers if date in workers_availability[worker]]
+        # sort workers by the max work days devided by the number of days they can work. pay attention to devided by zero
+        available_workers_sorted = sorted(available_workers, key=lambda worker: len(workers_availability[worker]) / workers_max_work_days[worker] if workers_max_work_days[worker] > 0 else float('inf'))
         # for each experty in mandatory_experties, book the first worker that has this experty
         experties_to_book = mandatory_experties.copy()
         num_of_workers_booked = num_of_workers_per_day
+        # sort the experties from the rearest experty to the most common
+        # experties_sorted = sorted(experties_to_book, key=lambda experty: sum([1 for worker in available_workers_sorted if experty in workers_experties[worker]]))
         for experty in experties_to_book:
-            # todo sort the experties by the rearest experty
-            workers_with_experty = [worker for worker in workers_sorted if experty in workers_experties[worker]]
+            workers_with_experty = [worker for worker in available_workers_sorted if experty in workers_experties[worker]]
             while experties_to_book[experty] > 0:
                 if len(workers_with_experty) > 0:
                     worker = workers_with_experty[0]
-                    workers_sorted.remove(worker)
+                    available_workers_sorted.remove(worker)
                     workers_with_experty.remove(worker)
                     workers_max_work_days[worker] -= 1
                     # remove all the required experties on this date
                     for worker_experty in workers_experties[worker]:
                         experties_to_book[worker_experty] -= 1
                     num_of_workers_booked -= 1
+                    schedual[date]['workers'].append(worker)
                     print(worker, date)
                 else: 
                     print(f'no more workers with experty {experty} on date {date}')
@@ -97,18 +80,33 @@ def schedual_workers():
         # count all experties that could not be booked
         experties_not_booked = sum([experties_to_book[experty] for experty in experties_to_book if experties_to_book[experty] > 0])
         # book the rest of the workers
-        # todo: take the workers with the least experties
+        # todo: take workers with the least experties
         while num_of_workers_booked > experties_not_booked:
-            for worker in workers_sorted:
-                workers_sorted.remove(worker)
+            for worker in available_workers_sorted:
+                available_workers_sorted.remove(worker)
                 workers_max_work_days[worker] -= 1
+                # remove all the required experties on this date
+                for worker_experty in workers_experties[worker]:
+                    experties_to_book[worker_experty] -= 1
                 print(worker, date)
+                schedual[date]['workers'].append(worker)
                 num_of_workers_booked -= 1
 
         # remove the date from the availability
         for worker in workers:
             if date in workers_availability[worker]:
                 workers_availability[worker].remove(date)
+        
+        # find all the workers that could be replaced
+        for worker in schedual[date]['workers']:
+            is_replacable = True
+            for worker_experty in workers_experties[worker]:
+                # check if all the worker's experties are booked and have en extra worker with this experty
+                if experties_to_book[worker_experty] >= 0:
+                    is_replacable = False
+                    break
+            if is_replacable:
+                schedual[date]['replacble_workers'].append(worker)
 
 if __name__ == '__main__':
     schedual_workers()
